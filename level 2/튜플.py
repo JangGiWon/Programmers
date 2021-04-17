cc6796f458e03f0ddb2511dae40d709598198741
PlayL <36566286+JangGiWon@users.noreply.github.com>
2021-04-17 11:39:00 +0900

    Add files via upload

---
+++ level 2/튜플.py
@@ -0,0 +1,16 @@
+def solution(s):
+    answer = []
+    s = s[2:-2]
+    s = s.split('},{')
+    s.sort(key=len)
+
+    for i in s:
+        num = i.split(',')
+        for j in num:
+            if int(j) not in answer:
+                answer.append(int(j))
+
+    return answer
+
+
+print(solution("{{2},{2,1},{2,1,3},{2,1,3,4}}"))
