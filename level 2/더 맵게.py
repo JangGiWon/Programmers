bdf6b125a92effee9134d7f3d816e0a547174d8f
PlayL <36566286+JangGiWon@users.noreply.github.com>
2021-10-28 10:13:44 +0900

    Add files via upload

---
+++ level 2/더 맵게.py
@@ -0,0 +1,20 @@
+import heapq
+
+
+def solution(scoville, k):
+    answer = 0
+    heapq.heapify(scoville)
+
+    while scoville[0] < k:
+        mix = heapq.heappop(scoville) + (heapq.heappop(scoville) * 2)
+        heapq.heappush(scoville, mix)
+        answer += 1
+
+        if len(scoville) == 1 and scoville[0] < k:
+            return -1
+
+    return answer
+
+
+print(solution([1, 2, 3, 9, 10, 12], 7))
+
